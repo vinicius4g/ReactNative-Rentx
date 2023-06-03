08c71522d0f85c0f344c5b5743ea936734fef406
vinicius4g <viniciusdias1701@hotmail.com>
2023-06-03 00:22:39 -0300

    add Car details and components

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components/native';
+import { RFValue } from 'react-native-responsive-fontsize';
+import { RectButton, RectButtonProps } from 'react-native-gesture-handler';
+
+interface ButtonProps extends RectButtonProps {
+  color: string;
+}
+
+export const Container = styled(RectButton)<ButtonProps>`
+  width: 100%;
+
+  padding: 19px;
+  justify-content: center;
+  align-items: center;
+
+  background-color: ${({ color }) => color};
+`;
+
+export const Title = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.primary_500};
+  font-size: ${RFValue(15)}px;
+
+  color: ${({ theme }) => theme.colors.shape};
+`;
